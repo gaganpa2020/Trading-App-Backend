aec9af034e9a2477a33b02bde7e3c406d953cca4
Gagan <luck.gagan@gmail.com>
2021-04-01 19:05:56 -0400

    Adding autofac settings.

--- AuthenticationService/TradingCompany.AuthenticationService/Program.cs
+++ AuthenticationService/TradingCompany.AuthenticationService/Program.cs
@@ -1,14 +1,9 @@
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
 namespace TradingCompany.AuthenticationService
 {
+	using Autofac.Extensions.DependencyInjection;
+	using Microsoft.AspNetCore.Hosting;
+	using Microsoft.Extensions.Hosting;
+
 	public class Program
 	{
 		public static void Main(string[] args)
@@ -18,6 +13,7 @@ namespace TradingCompany.AuthenticationService
 
 		public static IHostBuilder CreateHostBuilder(string[] args) =>
 			Host.CreateDefaultBuilder(args)
+				.UseServiceProviderFactory(new AutofacServiceProviderFactory())
 				.ConfigureWebHostDefaults(webBuilder =>
 				{
 					webBuilder.UseStartup<Startup>();
--- AuthenticationService/TradingCompany.AuthenticationService/Startup.cs
+++ AuthenticationService/TradingCompany.AuthenticationService/Startup.cs
@@ -1,18 +1,13 @@
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
 namespace TradingCompany.AuthenticationService
 {
+	using Autofac;
+	using Microsoft.AspNetCore.Builder;
+	using Microsoft.AspNetCore.Hosting;
+	using Microsoft.Extensions.Configuration;
+	using Microsoft.Extensions.DependencyInjection;
+	using Microsoft.Extensions.Hosting;
+	using TradingCompany.Shared;
+
 	public class Startup
 	{
 		public Startup(IConfiguration configuration)
@@ -57,6 +52,12 @@ namespace TradingCompany.AuthenticationService
 			{
 				endpoints.MapControllers();
 			});
+		}
+
+		public void ConfigureContainer(ContainerBuilder builder)
+		{
+			builder.RegisterType<CustomCache>().As<ICustomCache>();
+			builder.RegisterType<HttpProvider>().As<IProvider>();
 		}
 	}
 }
