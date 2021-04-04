b7819b88d908f5a4161afdf674b496a92ec1191d
Gagan <luck.gagan@gmail.com>
2021-04-04 12:29:42 -0400

    Pushing changes for transaction history.

--- TradingHistoryService/TradingCompany.TransactionHistory/TradingCompany.TransactionHistory/DependencyInjection.cs
+++ TradingHistoryService/TradingCompany.TransactionHistory/TradingCompany.TransactionHistory/DependencyInjection.cs
@@ -1,6 +1,7 @@
 ﻿namespace TradingCompany.UserService
 {
 	using Autofac;
+	using TradingCompany.Repository;
 	using TradingCompany.Shared;
 
 	public static class DependencyInjection
@@ -9,6 +10,7 @@
 		{
 			builder.RegisterType<CustomCache>().As<ICustomCache>();
 			builder.RegisterType<HttpProvider>().As<IProvider>();
+			builder.RegisterType<TransactionHistoryRepository>().As<ITransactionHistoryRepository>();
 		}
 	}
 }
